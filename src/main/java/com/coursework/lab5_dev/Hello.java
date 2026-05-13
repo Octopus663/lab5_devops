c490828d7c4e086388e6f2a27c9344b389ed17b5
KundaAndrii <145130888+Octopus663@users.noreply.github.com>
2026-05-13 23:17:48 +0300

    init commit 5 lab

---
+++ src/main/java/com/coursework/lab5_dev/Hello.java
@@ -0,0 +1,12 @@
+package com.coursework.lab5_dev;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping("/")
+    public String index() {
+        return "DevOps Lab is Running!";
+    }
+}
